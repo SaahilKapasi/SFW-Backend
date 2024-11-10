ea0ef554eb5d14f1a945cda72266d6b0b2ebb879
Saahil <183757326+SaahilKapasi@users.noreply.github.com>
2024-11-10 17:38:14 -0500

    Bring current frontend into project

---
+++ frontend/src/app/newpages/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useSearchParams } from 'next/navigation';
+
+export default function NewsletterPage() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
+
+  return (
+    <div>
+      <h1>Newsletter {id}</h1>
+    </div>
+  );
+};
